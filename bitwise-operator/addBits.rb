f241bedf8b5d7c58261c4dd482f154df12376a4f
Sotatek-MinhNguyen <minh.nguyen8@sotatek.com>
2022-05-14 20:25:39 +1000

    fix

--- bitwise-operator/addBits.rb
+++ bitwise-operator/addBits.rb
@@ -2,11 +2,5 @@ require_relative("./modifyBits");
 
 def addBit (x,y)
     k = (x + y) % (2 ** 32)
-    # k = k.to_s(2)
-    # puts "addBit " + modifyBits(k)
     return k
 end
-# y = 0b00000100000000000000000000000000
-# x = 0b00000000000000000011111111111111 
-
-# puts addBit(x,y);
